0cfcc80e681c533d62c8e6e7a2fd14c174d0c69a
KostyaMuroniuk <muronandrey2@gmail.com>
2026-06-13 10:34:35 +0300

    Feature: email smtp client factory added + unit test for sending

--- src/Email.Worker/Program.cs
+++ src/Email.Worker/Program.cs
@@ -1,6 +1,7 @@
 using Email.Worker.Consumers;
 using Email.Worker.Models;
 using Email.Worker.Services;
+using Email.Worker.Services.Smtp;
 using MassTransit;
 
 var builder = Host.CreateApplicationBuilder(args);
@@ -8,6 +9,8 @@ var builder = Host.CreateApplicationBuilder(args);
 builder.Services.Configure<EmailOptions>(builder.Configuration.GetSection("Email"));
 
 builder.Services.AddScoped<IEmailService, EmailSenderService>();
+
+builder.Services.AddSingleton<ISmtpClientFactory, SmtpClientFactory>();
 
 builder.Services.AddMassTransit(x =>
 {
---
+++ src/Email.Worker/Services/Smtp/ISmtpClientWrapper.cs
@@ -0,0 +1,16 @@
+﻿using MailKit.Security;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Email.Worker.Services.Smtp
+{
+    public interface ISmtpClientWrapper : IAsyncDisposable
+    {
+        Task ConnectAsync(string host, int port, SecureSocketOptions options, CancellationToken ct);
+        Task AuthenticateAsync(string userName, string password, CancellationToken ct);
+        Task SendAsync(MimeMessage message, CancellationToken ct);
+        Task DisconnectAsync(bool quit, CancellationToken ct);
+    }
+}
